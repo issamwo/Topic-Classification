de00f69661fa045e485bac9c6166cd62e67e7e9a
Nana Issam <issam.nana@inetum.com>
2023-12-27 09:15:44 +0100

    updated project template

--- template.py
+++ template.py
@@ -5,7 +5,7 @@ import logging
 #logging string
 logging.basicConfig(level=logging.INFO, format='[%(asctime)s]: %(message)s:')
 
-project_name = 'cnnClassifier'
+project_name = 'Classifier'
 
 list_of_files = [
     ".github/workflows/.gitkeep",
